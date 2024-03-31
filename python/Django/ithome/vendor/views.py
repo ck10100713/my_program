b5f8ef3fe9f6ac4218035f65b43b1fcb4dd1bbab
pckuo <b04201003@gmail.com>
2024-03-31 22:56:12 +0800

    revise views class name

--- python/Django/ithome/vendor/views.py
+++ python/Django/ithome/vendor/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Vendor
 
 # Create your views here.
-def showtemplate(request):
+def vendor_index(request):
     vendor_list = Vendor.objects.all()
     context = {'vendor_list': vendor_list}
     return render(request, 'test.html', context)
