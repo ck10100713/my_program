8a51078e14d6ef6d8bf4d86d8198e2c98480fb7b
pckuo <b04201003@gmail.com>
2024-04-01 22:01:57 +0800

    fix name problem

--- python/Django/ithome/vendor/views.py
+++ python/Django/ithome/vendor/views.py
@@ -6,9 +6,10 @@ from .forms import VendorForm, RawVendorForm
 def vendor_index(request):
     vendor_list = Vendor.objects.all()
     context = {'vendor_list': vendor_list}
-    return render(request, 'vendor\detail.html', context)
+    return render(request, 'vendor/detail.html', context)
 
 def vendor_create_view(request):
+
     form = RawVendorForm(request.POST or None)
     if form.is_valid():
         Vendor.objects.create(**form.cleaned_data)
@@ -18,8 +19,6 @@ def vendor_create_view(request):
     return render(request, 'vendor/vendor_create.html', context)
 
 def single_Vendor(request, id):
-    print(Vendor.objects.all())
-    vendor_list = Vendor.objects.get(id=id)
-    context = {'vendor_list': vendor_list}
-    print(context)
+    vendor_lists = Vendor.objects.get(id=id)
+    context = {'vendor_list': vendor_lists}
     return render(request, 'vendor/vendor_detail.html', context)
